d9410baf6a57be56a3bcc7f15c623f8d4256606c
André Lins <andrelucas01@hotmail.com>
2020-04-07 21:32:36 -0300

    Inserting loader in PostScreen

--- src/screens/Post/index.js
+++ src/screens/Post/index.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 
 import Root from 'components/Root';
+import Loader from 'components/Loader';
 import CardPost from 'containers/CardPost';
 import CardComment from './components/CardComment';
 
@@ -20,16 +21,23 @@ const PostScreen = ({ match, currentUser }) => {
   }, [match]);
 
   if (loading) {
-    return 'Carregando...';
+    return <Loader />;
   }
 
   return (
     <Root>
       <CardPost post={post} currentUser={currentUser} />
-      <h5>Comentários: </h5>
-      {post.comments.map(item => (
-        <CardComment key={item.text + item.date + item.author} comment={item} />
-      ))}
+      {post.comments && (
+        <>
+          <h5>Comentários: </h5>
+          {post.comments.map(item => (
+            <CardComment
+              key={item.text + item.date + item.author}
+              comment={item}
+            />
+          ))}
+        </>
+      )}
     </Root>
   );
 };
